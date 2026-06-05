ff6d6def2cbf9dd512e48c27b913a354204ec070
samircahudhary <9761063545samir@gmail.com>
2026-06-05 20:20:26 +0530

    Fix Vite allowed hosts

--- Resume-frontend/src/services/api.jsx
+++ Resume-frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
 })
 
 // Attach Clerk JWT token to every request
